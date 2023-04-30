96778965f7c563c65a08af4d22d11a86f69cabfe
Tom Krug <tom.krug@nanedge.com>
2023-04-30 17:34:12 -0400

    Add Multi-tier navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,34 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory(); // A copy of the Browser History OBJECT
 
+  // When a user clicks link governed by Marketing (Memory History)
+  // 1. Communicate change up to Container
+  // 2. Container's Browser History should update its current path
+
+  /**
+   * onNavigate gets called every time some navigation occurs in marketing app
+   * Determines where marketing app navigated to and pass that to container
+   */
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        // Prevent infinite loop by ensuring new URL is not same as current URL
+        if (pathname !== nextPathname) {
+          // Tell Browser History to navigate to this new URL
+          history.push(nextPathname); 
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
