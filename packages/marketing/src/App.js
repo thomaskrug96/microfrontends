90c1bc05109452d22468f9f365cfcb1323197987
Tom Krug <tom.krug@nanedge.com>
2023-05-04 09:03:53 -0400

    Remove main.css from /public, delete unused comps

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
-import Test from './components/Test';
 import Hero from './components/Hero';
 
 const generateClassName = createGenerateClassName({
